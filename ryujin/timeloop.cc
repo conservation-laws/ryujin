642e292e99fb4a2cb399c6aa3e48f578692b6bf9
Matthias Maier <tamiko@43-1.org>
2018-10-02 17:24:38 -0500

    instantiate 1D and 3D as well

--- ryujin/timeloop.cc
+++ ryujin/timeloop.cc
@@ -3,7 +3,7 @@
 namespace ryujin
 {
   /* instantiations */
-  //template class TimeLoop<1>;
+  template class TimeLoop<1>;
   template class TimeLoop<2>;
-  //template class TimeLoop<3>;
+  template class TimeLoop<3>;
 } // namespace ryujin
