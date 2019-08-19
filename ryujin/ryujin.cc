eb4ddb301b2698cf4a2c9cc52c868ac7177e9ea3
Matthias Maier <tamiko@43-1.org>
2019-08-19 17:29:01 -0500

    bähm

--- ryujin/ryujin.cc
+++ ryujin/ryujin.cc
@@ -9,7 +9,7 @@ int main (int argc, char *argv[])
   dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv);
   MPI_Comm mpi_communicator(MPI_COMM_WORLD);
 
-  ryujin::TimeLoop<DIM> time_loop(mpi_communicator);
+  ryujin::TimeLoop<DIM, NUMBER> time_loop(mpi_communicator);
 
   /* If necessary, create empty parameter file and exit: */
   dealii::ParameterAcceptor::initialize("ryujin.prm");
