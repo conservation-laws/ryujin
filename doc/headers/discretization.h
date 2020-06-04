d353f1923325342f9ebf818a2a80f6dd0013178e
Matthias Maier <tamiko@43-1.org>
2020-06-04 17:09:53 -0500

    reintroduce geometry_helper functions in namespace GridGenerator::

--- doc/headers/discretization.h
+++ doc/headers/discretization.h
@@ -1,5 +1,6 @@
 /**
  * @defgroup Discretization Discretization
  *
- *
+ * Several classes and helper functions for creating a number of benchmark
+ * configurations and controlling the finite element discretization.
  */
