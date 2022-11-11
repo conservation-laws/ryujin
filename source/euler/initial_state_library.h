1bc9a94f222e542da3b9f1c97c6288f158b47b30
Matthias Maier <tamiko@43-1.org>
2022-11-11 16:53:28 -0600

    Euler: Refactor into own namespace

--- source/euler/initial_state_library.h
+++ source/euler/initial_state_library.h
@@ -18,32 +18,35 @@
 
 namespace ryujin
 {
-  namespace InitialStateLibrary
+  namespace Euler
   {
-    /**
-     * Populate a given container with all initial state defined in this
-     * namespace
-     *
-     * @ingroup InitialValues
-     */
-    template <int dim, typename Number, typename T>
-    void populate_initial_state_list(T &initial_state_list,
-                                     const HyperbolicSystem &h,
-                                     const std::string &s)
+    namespace InitialStateLibrary
     {
-      using state_type = HyperbolicSystem::state_type<dim, Number>;
+      /**
+       * Populate a given container with all initial state defined in this
+       * namespace
+       *
+       * @ingroup InitialValues
+       */
+      template <int dim, typename Number, typename T>
+      void populate_initial_state_list(T &initial_state_list,
+                                       const HyperbolicSystem &h,
+                                       const std::string &s)
+      {
+        using state_type = HyperbolicSystem::state_type<dim, Number>;
 
-      auto add = [&](auto &&object) {
-        initial_state_list.emplace(std::move(object));
-      };
+        auto add = [&](auto &&object) {
+          initial_state_list.emplace(std::move(object));
+        };
 
-      add(std::make_unique<BeckerSolution<dim, Number, state_type>>(h, s));
-      add(std::make_unique<Contrast<dim, Number, state_type>>(h, s));
-      add(std::make_unique<IsentropicVortex<dim, Number, state_type>>(h, s));
-      add(std::make_unique<RampUp<dim, Number, state_type>>(h, s));
-      add(std::make_unique<ShockFront<dim, Number, state_type>>(h, s));
-      add(std::make_unique<Uniform<dim, Number, state_type>>(h, s));
-    }
+        add(std::make_unique<BeckerSolution<dim, Number, state_type>>(h, s));
+        add(std::make_unique<Contrast<dim, Number, state_type>>(h, s));
+        add(std::make_unique<IsentropicVortex<dim, Number, state_type>>(h, s));
+        add(std::make_unique<RampUp<dim, Number, state_type>>(h, s));
+        add(std::make_unique<ShockFront<dim, Number, state_type>>(h, s));
+        add(std::make_unique<Uniform<dim, Number, state_type>>(h, s));
+      }
 
-  } // namespace InitialStateLibrary
+    } // namespace InitialStateLibrary
+  } // namespace Euler
 } // namespace ryujin
