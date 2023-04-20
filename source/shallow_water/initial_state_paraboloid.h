b5b063dbc03c2690cb327593b3336e2b24da5296
ejtovar <ejtovar1@gmail.com>
2023-04-20 09:46:06 -0600

    modify shallow water paraboloid initial state to include 1D

--- source/shallow_water/initial_state_paraboloid.h
+++ source/shallow_water/initial_state_paraboloid.h
@@ -13,9 +13,10 @@ namespace ryujin
   namespace ShallowWater
   {
     /**
-     * A 2D Paraboloid configuration, see W. C. Thacker, Some exact
-     * solution to the nonlinear shallo-water wave equations, J. Fluid.
-     * Mech., 107:499-508, 1981.
+     * A 1D/2D Paraboloid configuration. See following reference:
+     *
+     * W. C. Thacker, Some exact solution to the nonlinear shallo-water wave
+     * equations, J. Fluid. Mech., 107:499-508, 1981.
      *
      * @ingroup InitialValues
      */
@@ -28,7 +29,7 @@ namespace ryujin
           : InitialState<dim, Number, state_type, 1>("paraboloid", subsection)
           , hyperbolic_system(hyperbolic_system)
       {
-        a_ = 1;
+        a_ = 1.;
         this->add_parameter(
             "free surface radius", a_, "Radius of the circular free surface");
 
@@ -42,29 +43,58 @@ namespace ryujin
 
       state_type compute(const dealii::Point<dim> &point, Number t) final
       {
-        if constexpr (dim == 1) {
+        /* Initialize primitive variables */
+        Number h = 0.;   // water depth
+        Number v_x = 0.; // velocity in x-direction
+        Number v_y = 0.; // velocity in y-direction (if in 2D)
+
+        /* Common quantities */
+        const auto z = compute_bathymetry(point);
+        const auto g = hyperbolic_system.gravity();
+        const Number omega = std::sqrt(2. * g * h_0_) / a_;
+
+        /* Define profiles for each dimension */
+        switch (dim) {
+        case 1:
+          /* Fake 1D configuration */
+          {
+            const Number &x = point[0];
+
+            h = eta_ * h_0_ / (a_ * a_) * (2. * x * std::cos(omega * t)) - z;
+            h = std::max(h, Number(0.));
+
+            v_x = -eta_ * omega * std::sin(omega * t);
+          }
+          break;
+
+        case 2:
+          /* 2D configuration as described in reference above */
+          {
+            const Number &x = point[0];
+            const Number &y = point[1];
+
+            h = eta_ * h_0_ / (a_ * a_) *
+                    (2. * x * std::cos(omega * t) +
+                     2 * y * std::sin(omega * t)) -
+                z;
+            h = std::max(h, Number(0.));
+
+            v_x = -eta_ * omega * std::sin(omega * t);
+            v_y = eta_ * omega * std::cos(omega * t);
+          }
+          break;
+        }
+
+        /* Set final state */
+        if constexpr (dim == 1)
+          return hyperbolic_system.template expand_state<dim>(
+              HyperbolicSystem::state_type<1, Number>{{h, h * v_x}});
+        else if constexpr (dim == 2)
+          return hyperbolic_system.template expand_state<dim>(
+              HyperbolicSystem::state_type<1, Number>{{h, h * v_x, h * v_y}});
+        else {
           AssertThrow(false, dealii::ExcNotImplemented());
           __builtin_trap();
-          return state_type();
-
-        } else {
-          const auto z = compute_bathymetry(point);
-          const auto g = hyperbolic_system.gravity();
-          const Number &x = point[0];
-          const Number &y = point[1];
-          const Number omega = std::sqrt(2. * g * h_0_) / a_;
-
-          Number h =
-              eta_ * h_0_ / (a_ * a_) *
-                  (2. * x * std::cos(omega * t) + 2 * y * std::sin(omega * t)) -
-              z;
-          h = std::max(h, Number(0.));
-
-          const Number v_x = -eta_ * omega * std::sin(omega * t);
-          const Number v_y = eta_ * omega * std::cos(omega * t);
-
-          return hyperbolic_system.template expand_state<dim>(
-              HyperbolicSystem::state_type<2, Number>{{h, h * v_x, h * v_y}});
         }
       }
 
