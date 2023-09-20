db884c42a4f40371d53c94e0616e4b5252d717d1
Matthias Maier <tamiko@43-1.org>
2023-09-20 17:06:22 -0500

    Explicitly instantiate hyperbolic and parabolic step functions in respective compilation units

--- source/hyperbolic_module.cc
+++ source/hyperbolic_module.cc
@@ -6,11 +6,40 @@
 #include "hyperbolic_module.template.h"
 #include <instantiate.h>
 
+#define INSTANTIATE(dim, stages)                                               \
+  template NUMBER HyperbolicModule<Description, dim, NUMBER>::step<stages>(    \
+      const vector_type &,                                                     \
+      std::array<std::reference_wrapper<const vector_type>, stages>,           \
+      std::array<std::reference_wrapper<const precomputed_vector_type>,        \
+                 stages>,                                                      \
+      const std::array<NUMBER, stages>,                                        \
+      vector_type &,                                                           \
+      precomputed_vector_type &,                                               \
+      NUMBER) const
+
 namespace ryujin
 {
   /* instantiations */
+
   template class HyperbolicModule<Description, 1, NUMBER>;
   template class HyperbolicModule<Description, 2, NUMBER>;
   template class HyperbolicModule<Description, 3, NUMBER>;
 
+  INSTANTIATE(1, 0);
+  INSTANTIATE(1, 1);
+  INSTANTIATE(1, 2);
+  INSTANTIATE(1, 3);
+  INSTANTIATE(1, 4);
+
+  INSTANTIATE(2, 0);
+  INSTANTIATE(2, 1);
+  INSTANTIATE(2, 2);
+  INSTANTIATE(2, 3);
+  INSTANTIATE(2, 4);
+
+  INSTANTIATE(3, 0);
+  INSTANTIATE(3, 1);
+  INSTANTIATE(3, 2);
+  INSTANTIATE(3, 3);
+  INSTANTIATE(3, 4);
 } /* namespace ryujin */
--- source/parabolic_module.cc
+++ source/parabolic_module.cc
@@ -6,11 +6,26 @@
 #include "parabolic_module.template.h"
 #include <instantiate.h>
 
+#define INSTANTIATE(dim, stages)                                               \
+  template void ParabolicModule<Description, dim, NUMBER>::step<stages>(       \
+      const vector_type &,                                                     \
+      const NUMBER,                                                            \
+      std::array<std::reference_wrapper<const vector_type>, stages>,           \
+      const std::array<NUMBER, stages>,                                        \
+      vector_type &,                                                           \
+      const NUMBER) const
+
 namespace ryujin
 {
   /* instantiations */
   template class ParabolicModule<Description, 1, NUMBER>;
   template class ParabolicModule<Description, 2, NUMBER>;
   template class ParabolicModule<Description, 3, NUMBER>;
+
+  INSTANTIATE(1, 0);
+
+  INSTANTIATE(2, 0);
+
+  INSTANTIATE(3, 0);
 
 } /* namespace ryujin */
