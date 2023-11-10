b3eafdfa51a0be95ec4096d10412f830f3314488
Eric Tovar <ejtovar1@gmail.com>
2023-11-10 17:17:48 -0600

    ShallowWater: implement indicator

--- source/shallow_water/indicator.h
+++ source/shallow_water/indicator.h
@@ -32,6 +32,18 @@ namespace ryujin
       using HyperbolicSystemView = HyperbolicSystem::View<dim, Number>;
 
       /**
+       * @copydoc HyperbolicSystem::problem_dimension
+       */
+      static constexpr unsigned int problem_dimension =
+          HyperbolicSystemView::problem_dimension;
+
+      /**
+       * @copydoc HyperbolicSystem::precomputed_state_type
+       */
+      using precomputed_state_type =
+          typename HyperbolicSystemView::precomputed_state_type;
+
+      /**
        * @copydoc HyperbolicSystem::n_precomputed_values
        */
       static constexpr unsigned int n_precomputed_values =
@@ -86,29 +98,20 @@ namespace ryujin
        * Reset temporary storage and initialize for a new row corresponding
        * to state vector U_i.
        */
-      void reset(const unsigned int /*i*/, const state_type & /*U_i*/)
-      {
-        // empty
-      }
+      void reset(const unsigned int /*i*/, const state_type &U_i);
 
       /**
        * When looping over the sparsity row, add the contribution associated
        * with the neighboring state U_j.
        */
-      void accumulate(const unsigned int * /*js*/,
-                      const state_type & /*U_j*/,
-                      const dealii::Tensor<1, dim, Number> & /*c_ij*/)
-      {
-        // empty
-      }
+      void accumulate(const unsigned int *js,
+                      const state_type &U_j,
+                      const dealii::Tensor<1, dim, Number> &c_ij);
 
       /**
        * Return the computed alpha_i value.
        */
-      Number alpha(const Number /*h_i*/)
-      {
-        return Number(0.);
-      }
+      Number alpha(const Number h_i);
 
       //@}
 
@@ -121,7 +124,94 @@ namespace ryujin
 
       const MultiComponentVector<ScalarNumber, n_precomputed_values>
           &precomputed_values;
+
+      Number h_i = 0.;
+      Number eta_i = 0.;
+      flux_type f_i;
+      state_type d_eta_i;
+      Number pressure_i = 0.;
+
+      Number left = 0.;
+      state_type right;
       //@}
     };
+
+
+    /*
+     * -------------------------------------------------------------------------
+     * Inline definitions
+     * -------------------------------------------------------------------------
+     */
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline void
+    Indicator<dim, Number>::reset(const unsigned int i, const state_type &U_i)
+    {
+      h_i = hyperbolic_system.water_depth(U_i);
+      /* entropy viscosity commutator: */
+
+      const auto &[mathematical_entropy] =
+          precomputed_values
+              .template get_tensor<Number, precomputed_state_type>(i);
+
+      eta_i = mathematical_entropy;
+
+      d_eta_i = hyperbolic_system.mathematical_entropy_derivative(U_i);
+      f_i = hyperbolic_system.f(U_i);
+      pressure_i = hyperbolic_system.pressure(U_i);
+
+      left = 0.;
+      right = 0.;
+    }
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline void Indicator<dim, Number>::accumulate(
+        const unsigned int *js,
+        const state_type &U_j,
+        const dealii::Tensor<1, dim, Number> &c_ij)
+    {
+      /* entropy viscosity commutator: */
+
+      const auto &[eta_j] =
+          precomputed_values
+              .template get_tensor<Number, precomputed_state_type>(js);
+
+      const auto velocity_j = hyperbolic_system.momentum(U_j) *
+                              hyperbolic_system.inverse_water_depth_sharp(U_j);
+      const auto f_j = hyperbolic_system.f(U_j);
+      const auto pressure_j = hyperbolic_system.pressure(U_j);
+
+      left += (eta_j + pressure_j) * (velocity_j * c_ij);
+
+      for (unsigned int k = 0; k < problem_dimension; ++k)
+        right[k] += (f_j[k] - f_i[k]) * c_ij;
+    }
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline Number
+    Indicator<dim, Number>::alpha(const Number hd_i)
+    {
+      Number my_sum = 0.;
+      for (unsigned int k = 0; k < problem_dimension; ++k) {
+        my_sum += d_eta_i[k] * right[k];
+      }
+
+      Number numerator = std::abs(left - my_sum);
+      Number denominator = std::abs(left) + std::abs(my_sum);
+
+      constexpr ScalarNumber eps = std::numeric_limits<ScalarNumber>::epsilon();
+      const auto quotient = std::abs(numerator + eps) /
+                            (denominator + hd_i * std::abs(eta_i) + eps);
+
+      /* FIXME: this can be refactoring into a runtime parameter... */
+      const ScalarNumber evc_alpha_0_ = ScalarNumber(1.);
+
+      return std::min(Number(1.), evc_alpha_0_ * quotient);
+    }
+
+
   } // namespace ShallowWater
 } // namespace ryujin
