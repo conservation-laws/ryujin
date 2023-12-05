6fbfbfe57a3cc1cb8cf3ca409a56b0a82f48c243
Eric Tovar <ejtovar1@gmail.com>
2023-12-05 15:31:22 -0700

    ShallowWater: modify paraboloid initial state to have a different 1D state

--- source/shallow_water/initial_state_paraboloid.h
+++ source/shallow_water/initial_state_paraboloid.h
@@ -16,7 +16,10 @@ namespace ryujin
      * paraboloid basin without friction. See Section 4.2.2 of
      * @cite swashes_2013 for details.
      *
-     * @todo Add variation with friction.
+     * @note The 1D variation is a different than the reference above and has
+     * frictions effects.
+     *
+     * @todo Add variation with friction for 2D case.
      *
      * @ingroup ShallowWaterEquations
      */
@@ -42,8 +45,19 @@ namespace ryujin
         this->add_parameter(
             "water height", h_0_, "Water height at central point");
 
-        eta_ = 0.5;
-        this->add_parameter("eta", eta_, "The eta parameter");
+        if constexpr (dim == 1) {
+
+          length_ = 10000.;
+          this->add_parameter(
+              "paraboloid length", length_, "Length of 1D paraboloid");
+
+          B_ = 2.;
+          this->add_parameter("speed", B_, "The 1D paraboloid speed");
+
+        } else {
+          eta_ = 0.5;
+          this->add_parameter("eta", eta_, "The eta parameter");
+        }
       }
 
       state_type compute(const dealii::Point<dim> &point, Number t) final
@@ -57,14 +71,30 @@ namespace ryujin
         /* Initialize primitive variables */
         Number h, v_x, v_y = 0.;
 
-        /* Define profiles for each dimension */
+        /* Define slightly different profiles for each dimension */
+
         if constexpr (dim == 1) {
 
-          const auto elevation =
-              eta_ * h_0_ / (a_ * a_) * (2. * x * std::cos(omega * t));
+          const Number k = hyperbolic_system_.manning_friction_coefficient();
+          const Number p = std::sqrt(8. * g * h_0_) / a_;
+          const Number s = std::sqrt(p * p - k * k) / 2.;
 
-          h = std::max(elevation - z, Number(0.));
-          v_x = -eta_ * omega * std::sin(omega * t);
+          auto term1 =
+              (a_ * a_ * B_ * B_) / (8. * g * g * h_0_) * std::exp(-k * t);
+          term1 *= (1. / 4. * k * k - s * s) * std::cos(2. * s * t) -
+                   s * k * sin(2. * s * t);
+
+          const auto term2 = -(B_ * B_ / (4. * g)) * std::exp(-k * t);
+
+          auto term3 = -(B_ / g) * std::exp(-1. / 2. * k * t);
+          term3 *= (s * std::cos(s * t) + 1. / 2. * k * std::sin(s * t)) *
+                   (point[0] - 1. / 2. * length_);
+
+          auto htilde = h_0_ - compute_bathymetry(point);
+          htilde += term1 + term2 + term3;
+
+          h = std::max(htilde, 0.);
+          v_x = B_ * std::exp(-1. / 2. * k * t) * std::sin(s * t);
 
           return state_type{{h, h * v_x}};
         } else if constexpr (dim == 2) {
@@ -102,12 +132,17 @@ namespace ryujin
       DEAL_II_ALWAYS_INLINE inline Number
       compute_bathymetry(const dealii::Point<dim> &point) const
       {
-        return -h_0_ * (Number(1.) - point.norm_square() / (a_ * a_));
+        if constexpr (dim == 1)
+          return h_0_ / (a_ * a_) * std::pow(point[0] - 0.5 * length_, 2);
+        else
+          return -h_0_ * (Number(1.) - point.norm_square() / (a_ * a_));
       }
 
       Number a_;
       Number h_0_;
       Number eta_;
+      Number length_;
+      Number B_;
     };
 
   } // namespace ShallowWaterInitialStates
