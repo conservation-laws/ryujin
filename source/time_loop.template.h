acf9d492d695a02938c43c97349a18f765d6c154
Matthias Maier <tamiko@43-1.org>
2024-07-28 19:03:20 -0500

    TimeLoop: broadcast boolean instead of computing expensive average

--- source/time_loop.template.h
+++ source/time_loop.template.h
@@ -364,22 +364,22 @@ namespace ryujin
       t += tau;
 
       /* Print and record cycle statistics: */
-      {
+      if (terminal_update_interval_ != Number(0.)) {
         const bool write_to_log_file = (t >= timer_cycle * timer_granularity_);
 
-        /* Synchronize average Wall time over all MPI ranks: */
         const auto wall_time = computing_timer_["time loop"].wall_time();
-        const auto average =
-            Utilities::MPI::min_max_avg(wall_time, mpi_communicator_).avg;
-        const bool update_terminal =
-            (average >= last_terminal_output + terminal_update_interval_);
+        bool update_terminal =
+            (wall_time >= last_terminal_output + terminal_update_interval_);
 
-        if (terminal_update_interval_ != Number(0.)) {
-          if (write_to_log_file || update_terminal) {
-            print_cycle_statistics(
-                cycle, t, timer_cycle, /*logfile*/ write_to_log_file);
-            last_terminal_output = average;
-          }
+        /* Broadcast boolean from rank 0 to all other ranks: */
+        const auto ierr =
+            MPI_Bcast(&update_terminal, 1, MPIU_BOOL, 0, mpi_communicator_);
+        AssertThrowMPI(ierr);
+
+        if (write_to_log_file || update_terminal) {
+          print_cycle_statistics(
+              cycle, t, timer_cycle, /*logfile*/ write_to_log_file);
+          last_terminal_output = wall_time;
         }
       }
     } /* end of loop */
