8804752045760c98076ce76d70c2602a41059d1f
Matthias Maier <tamiko@43-1.org>
2025-07-31 23:46:45 -0500

    TimeLoop: improve the fudging of CPU timings
    Only scale the CPU timings in case of hyperthreading, i.e., precisely 2
    threads per rank (and enabled parameter).

--- source/time_loop.template.h
+++ source/time_loop.template.h
@@ -196,14 +196,13 @@ namespace ryujin
                   "recomputed and printed on the terminal. Setting the "
                   "interval to zero disables terminal output.");
 
-    terminal_show_rank_throughput_ = true;
-    add_parameter("terminal show rank throughput",
-                  terminal_show_rank_throughput_,
-                  "If set to true an average per rank throughput is computed "
-                  "by dividing the total consumed CPU time (per rank) by the "
-                  "number of threads (per rank). If set to false then a plain "
-                  "average per thread \"CPU\" throughput value is computed by "
-                  "using the umodified total accumulated CPU time.");
+    terminal_correct_for_hypertreadhing_ = true;
+    add_parameter(
+        "terminal correct for hyperthreading",
+        terminal_correct_for_hypertreadhing_,
+        "If set to true, the CPU throughput is corrected by dividing the total "
+        "consumed CPU time by a factor of 2. This correction is only active if "
+        "the number of threads (per MPI rank) is 2.");
 
     checkpoint_update_interval_ = 0;
     add_parameter(
@@ -1331,17 +1330,22 @@ namespace ryujin
     const auto efficiency = time_integrator_.efficiency();
     const auto n_dofs = static_cast<double>(n_global_dofs_);
 
-    const double wall_m_dofs_per_sec =
-        delta_cycles * n_dofs / 1.e6 /
-        (current.wall_time - previous.wall_time) * efficiency;
+    double wall_m_dofs_per_sec = delta_cycles * n_dofs * efficiency / 1.e6 /
+                                 (current.wall_time - previous.wall_time);
 
-    double cpu_m_dofs_per_sec = delta_cycles * n_dofs / 1.e6 /
-                                (current.cpu_time_sum - previous.cpu_time_sum) *
-                                efficiency;
+    double cpu_m_dofs_per_sec = delta_cycles * n_dofs * efficiency / 1.e6 /
+                                (current.cpu_time_sum - previous.cpu_time_sum);
+
+    /* Determine whether we fudge the CPU timings: */
+    const bool fudge_cpu_timings = terminal_correct_for_hypertreadhing_ &&
 #ifdef WITH_OPENMP
-    if (terminal_show_rank_throughput_)
-      cpu_m_dofs_per_sec *= omp_get_max_threads();
+                                   (omp_get_max_threads() == 2);
+#else
+                                   false;
 #endif
+
+    if (fudge_cpu_timings)
+      cpu_m_dofs_per_sec *= 2.;
 
     double cpu_time_skew = (current.cpu_time_max - current.cpu_time_min - //
                             previous.cpu_time_max + previous.cpu_time_min) /
@@ -1366,7 +1370,7 @@ namespace ryujin
     output << std::endl;
 
     output << "Throughput:\n  "
-           << (terminal_show_rank_throughput_? "RANK: " : "CPU : ")
+           << (fudge_cpu_timings ? "CPU*: " : "CPU : ")
            << std::setprecision(4) << std::fixed << cpu_m_dofs_per_sec
            << " MQ/s  ("
            << std::scientific << 1. / cpu_m_dofs_per_sec * 1.e-6
