3b7a94a622eea52df0a0e64a2c3eeb9c53454a62
Jordan Hoffart <jordanhoffart@tamu.edu>
2025-09-02 01:48:00 -0500

    EulerBarotropic: implement limiter

---
+++ source/euler_barotropic/limiter.cc
@@ -0,0 +1,24 @@
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (C) 2020 - 2023 by the ryujin authors
+//
+
+#include "limiter.template.h"
+
+using namespace dealii;
+
+namespace ryujin
+{
+  namespace EulerBarotropic
+  {
+    /* instantiations */
+
+    template class Limiter<1, NUMBER>;
+    template class Limiter<2, NUMBER>;
+    template class Limiter<3, NUMBER>;
+
+    template class Limiter<1, dealii::VectorizedArray<NUMBER>>;
+    template class Limiter<2, dealii::VectorizedArray<NUMBER>>;
+    template class Limiter<3, dealii::VectorizedArray<NUMBER>>;
+  } // namespace EulerBarotropic
+} // namespace ryujin
---
+++ source/euler_barotropic/limiter.h
@@ -0,0 +1,364 @@
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (C) 2020 - 2024 by the ryujin authors
+//
+
+#pragma once
+
+#include <compile_time_options.h>
+
+#include "hyperbolic_system.h"
+
+#include <multicomponent_vector.h>
+#include <newton.h>
+#include <simd.h>
+
+namespace ryujin
+{
+  namespace EulerBarotropic
+  {
+    template <typename ScalarNumber = double>
+    class LimiterParameters : public dealii::ParameterAcceptor
+    {
+    public:
+      LimiterParameters(const std::string &subsection = "/Limiter")
+          : ParameterAcceptor(subsection)
+      {
+        iterations_ = 2;
+        add_parameter(
+            "iterations", iterations_, "Number of limiter iterations");
+
+        relaxation_factor_ = ScalarNumber(1.);
+        add_parameter("relaxation factor",
+                      relaxation_factor_,
+                      "Factor for scaling the relaxation window with r_i = "
+                      "factor * (m_i/|Omega|)^(1.5/d).");
+      }
+
+      ACCESSOR_READ_ONLY(iterations);
+      ACCESSOR_READ_ONLY(relaxation_factor);
+
+    private:
+      unsigned int iterations_;
+      ScalarNumber relaxation_factor_;
+    };
+
+
+    /**
+     * The convex limiter.
+     *
+     * @ingroup EulerEquations
+     */
+    template <int dim, typename Number = double>
+    class Limiter
+    {
+    public:
+      /**
+       * @name Typedefs and constexpr constants
+       */
+      //@{
+
+      using View = HyperbolicSystemView<dim, Number>;
+
+      using ScalarNumber = typename View::ScalarNumber;
+
+      static constexpr auto problem_dimension = View::problem_dimension;
+
+      using state_type = typename View::state_type;
+
+      using flux_contribution_type = typename View::flux_contribution_type;
+
+      using precomputed_type = typename View::precomputed_type;
+
+      using PrecomputedVector = typename View::PrecomputedVector;
+
+      using Parameters = LimiterParameters<ScalarNumber>;
+
+      //@}
+      /**
+       * @name Computation and manipulation of bounds
+       */
+      //
+      //@{
+      /**
+       * The number of stored entries in the bounds array.
+       */
+      static constexpr unsigned int n_bounds = 2;
+
+      /**
+       * Array type used to store accumulated bounds.
+       */
+      using Bounds = std::array<Number, n_bounds>;
+
+      /**
+       * Constructor taking a HyperbolicSystem instance as argument
+       */
+      Limiter(const HyperbolicSystem &hyperbolic_system,
+              const Parameters &parameters,
+              const PrecomputedVector &precomputed_values)
+          : hyperbolic_system(hyperbolic_system)
+          , parameters(parameters)
+          , precomputed_values(precomputed_values)
+      {
+      }
+
+      /**
+       * Given a state @p U_i and an index @p i return "strict" bounds,
+       * i.e., a minimal convex set containing the state.
+       */
+      Bounds projection_bounds_from_state(const unsigned int i,
+                                          const state_type &U_i) const;
+
+      /**
+       * Given two bounds bounds_left, bounds_right, this function computes
+       * a larger, combined set of bounds that this is a (convex) superset
+       * of the two.
+       */
+      Bounds combine_bounds(const Bounds &bounds_left,
+                            const Bounds &bounds_right) const;
+
+      /**
+       * This function applies a relaxation to a given a (strict) bound @p
+       * bounds using a non dimensionalized measure @p hd (that should
+       * scale as $h^d$, where $h$ is the local mesh size). This is done
+       * for the case of the Euler equations by multiplying maximum bounds
+       * with $(1+r)$ and minimum bounds with $(1-r)$, while ensuring that
+       * the bounds still describe an admissible state.
+       */
+      Bounds fully_relax_bounds(const Bounds &bounds, const Number &hd) const;
+
+      //@}
+      /**
+       * @name Stencil-based computation of bounds
+       *
+       * Intended usage:
+       * ```
+       * Limiter<dim, Number> limiter;
+       * for (unsigned int i = n_internal; i < n_owned; ++i) {
+       *   // ...
+       *   limiter.reset(i, U_i, flux_i);
+       *   for (unsigned int col_idx = 1; col_idx < row_length; ++col_idx) {
+       *     // ...
+       *     limiter.accumulate(js, U_j, flux_j, scaled_c_ij, affine_shift);
+       *   }
+       *   limiter.bounds(hd_i);
+       * }
+       * ```
+       */
+      //@{
+
+      /**
+       * Reset temporary storage
+       */
+      void reset(const unsigned int i,
+                 const state_type &U_i,
+                 const flux_contribution_type &flux_i);
+
+      /**
+       * When looping over the sparsity row, add the contribution associated
+       * with the neighboring state U_j.
+       */
+      void accumulate(const unsigned int *js,
+                      const state_type &U_j,
+                      const flux_contribution_type &flux_j,
+                      const dealii::Tensor<1, dim, Number> &scaled_c_ij,
+                      const state_type &affine_shift);
+
+      /**
+       * Return the computed bounds (with relaxation applied).
+       */
+      Bounds bounds(const Number hd_i) const;
+
+      //*}
+      /** @name Convex limiter */
+      //@{
+
+      /**
+       * Given a state \f$\mathbf U\f$ and an update \f$\mathbf P\f$ this
+       * function computes and returns the maximal coefficient \f$t\f$,
+       * obeying \f$t_{\text{min}} < t < t_{\text{max}}\f$, such that the
+       * selected local minimum principles are obeyed.
+       *
+       * The returned boolean is set to true if the original low-order
+       * update was within bounds.
+       *
+       * @note If the debug option `DEBUG_EXPENSIVE_BOUNDS_CHECK` is set to
+       * true, then the boolean is set to true if the low-order and the
+       * resulting high-order update are within bounds. The latter might be
+       * violated due to round-off errors when computing the limiter
+       * bounds.
+       */
+      std::tuple<Number, bool> limit(const Bounds &bounds,
+                                     const state_type &U,
+                                     const state_type &P,
+                                     const Number t_min = Number(0.),
+                                     const Number t_max = Number(1.)) const;
+
+    private:
+      //@}
+      /** @name Arguments and internal fields */
+      //@{
+
+      const HyperbolicSystem &hyperbolic_system;
+      const Parameters &parameters;
+      const PrecomputedVector &precomputed_values;
+
+      state_type U_i;
+      flux_contribution_type flux_i;
+
+      Bounds bounds_;
+
+      Number rho_relaxation_numerator;
+      Number rho_relaxation_denominator;
+
+      //@}
+    };
+
+
+    /*
+     * -------------------------------------------------------------------------
+     * Inline definitions
+     * -------------------------------------------------------------------------
+     */
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline auto
+    Limiter<dim, Number>::projection_bounds_from_state(
+        const unsigned int /*i*/, const state_type &U_i) const -> Bounds
+    {
+      const auto view = hyperbolic_system.view<dim, Number>();
+      const auto rho_i = view.density(U_i);
+      return {/*rho_min*/ rho_i, /*rho_max*/ rho_i};
+    }
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline auto Limiter<dim, Number>::combine_bounds(
+        const Bounds &bounds_left, const Bounds &bounds_right) const -> Bounds
+    {
+      const auto &[rho_min_l, rho_max_l] = bounds_left;
+      const auto &[rho_min_r, rho_max_r] = bounds_right;
+
+      return {std::min(rho_min_l, rho_min_r), std::max(rho_max_l, rho_max_r)};
+    }
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline auto
+    Limiter<dim, Number>::fully_relax_bounds(const Bounds &bounds,
+                                             const Number &hd) const -> Bounds
+    {
+      auto relaxed_bounds = bounds;
+      auto &[rho_min_relaxed, rho_max_relaxed] = relaxed_bounds;
+
+      /* Use r = factor * (m_i / |Omega|) ^ (1.5 / d): */
+
+      Number r = std::sqrt(hd);                              // in 3D: ^ 3/6
+      if constexpr (dim == 2)                                //
+        r = dealii::Utilities::fixed_power<3>(std::sqrt(r)); // in 2D: ^ 3/4
+      else if constexpr (dim == 1)                           //
+        r = dealii::Utilities::fixed_power<3>(r);            // in 1D: ^ 3/2
+      r *= parameters.relaxation_factor();
+
+      constexpr ScalarNumber eps = std::numeric_limits<ScalarNumber>::epsilon();
+      rho_min_relaxed *= std::max(Number(1.) - r, Number(eps));
+      rho_max_relaxed *= (Number(1.) + r);
+
+      return relaxed_bounds;
+    }
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline void
+    Limiter<dim, Number>::reset(const unsigned int /*i*/,
+                                const state_type &new_U_i,
+                                const flux_contribution_type &new_flux_i)
+    {
+      U_i = new_U_i;
+      flux_i = new_flux_i;
+
+      /* Bounds: */
+
+      auto &[rho_min, rho_max] = bounds_;
+
+      rho_min = Number(std::numeric_limits<ScalarNumber>::max());
+      rho_max = Number(0.);
+
+      /* Relaxation: */
+
+      rho_relaxation_numerator = Number(0.);
+      rho_relaxation_denominator = Number(0.);
+    }
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline void Limiter<dim, Number>::accumulate(
+        const unsigned int * /*js*/,
+        const state_type &U_j,
+        const flux_contribution_type &flux_j,
+        const dealii::Tensor<1, dim, Number> &scaled_c_ij,
+        const state_type &affine_shift)
+    {
+      // TODO: Currently we only apply the affine_shift to U_ij_bar (which
+      // then enters all bounds), but we do not modify s_interp and
+      // rho_relaxation. When actually adding a source term to the Euler
+      // equations verify that this does the right thing.
+      Assert(std::max(affine_shift.norm(), Number(0.)) == Number(0.),
+             dealii::ExcNotImplemented());
+
+      const auto view = hyperbolic_system.view<dim, Number>();
+
+      /* Bounds: */
+      auto &[rho_min, rho_max] = bounds_;
+
+      const auto rho_i = view.density(U_i);
+      const auto rho_j = view.density(U_j);
+
+      /* bar state shifted by an affine shift: */
+      const auto U_ij_bar =
+          ScalarNumber(0.5) * (U_i + U_j) -
+          ScalarNumber(0.5) * contract(add(flux_j, -flux_i), scaled_c_ij) +
+          affine_shift;
+
+      const auto rho_ij_bar = view.density(U_ij_bar);
+
+      /* Density bounds: */
+
+      rho_min = std::min(rho_min, rho_ij_bar);
+      rho_max = std::max(rho_max, rho_ij_bar);
+
+      /* Density relaxation: */
+
+      /* Use a uniform weight. */
+      const auto beta_ij = Number(1.);
+      rho_relaxation_numerator += beta_ij * (rho_i + rho_j);
+      rho_relaxation_denominator += std::abs(beta_ij);
+    }
+
+
+    template <int dim, typename Number>
+    DEAL_II_ALWAYS_INLINE inline auto
+    Limiter<dim, Number>::bounds(const Number hd_i) const -> Bounds
+    {
+      const auto &[rho_min, rho_max] = bounds_;
+
+      auto relaxed_bounds = fully_relax_bounds(bounds_, hd_i);
+      auto &[rho_min_relaxed, rho_max_relaxed] = relaxed_bounds;
+
+      /* Apply a stricter window: */
+
+      constexpr ScalarNumber eps = std::numeric_limits<ScalarNumber>::epsilon();
+
+      const auto rho_relaxation =
+          ScalarNumber(2. * parameters.relaxation_factor()) *
+          std::abs(rho_relaxation_numerator) /
+          (std::abs(rho_relaxation_denominator) + Number(eps));
+
+      rho_min_relaxed = std::max(rho_min_relaxed, rho_min - rho_relaxation);
+      rho_max_relaxed = std::min(rho_max_relaxed, rho_max + rho_relaxation);
+
+      return relaxed_bounds;
+    }
+  } // namespace EulerBarotropic
+} // namespace ryujin
