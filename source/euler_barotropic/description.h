30770c56c85baaccca14eaa87aed73f410e0231b
Matthias Maier <tamiko@43-1.org>
2025-09-02 01:45:05 -0500

    EulerBarotropic: add infrastructure

---
+++ source/euler_barotropic/description.h
@@ -0,0 +1,58 @@
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (C) 2023 - 2024 by the ryujin authors
+//
+
+#pragma once
+
+#include <compile_time_options.h>
+
+#include "../stub_parabolic_system.h"
+#include "../stub_solver.h"
+#include "hyperbolic_system.h"
+#include "indicator.h"
+#include "limiter.h"
+#include "riemann_solver.h"
+
+namespace ryujin
+{
+  namespace EulerBarotropic
+  {
+    /**
+     * A struct that contains all equation specific classes describing the
+     * chosen hyperbolic system, the indicator, the limiter and
+     * (approximate) Riemann solver.
+     *
+     * The compressible Euler equations of gas dynamics. Specialized
+     * implementation for a subclass of barotropic equations of state where
+     * the pressure, internal energy and entropies are a function of the
+     * density. We use a specialied Riemann solver, entropy viscosity
+     * commutator, and limiter for this class of equations.
+     *
+     * The parabolic subsystem is chosen to be the identity.
+     *
+     * @ingroup EulerEquations
+     */
+    struct Description {
+      using HyperbolicSystem = EulerBarotropic::HyperbolicSystem;
+
+      template <int dim, typename Number = double>
+      using HyperbolicSystemView =
+          EulerBarotropic::HyperbolicSystemView<dim, Number>;
+
+      using ParabolicSystem = ryujin::StubParabolicSystem;
+
+      template <int dim, typename Number = double>
+      using ParabolicSolver = ryujin::StubSolver<Description, dim, Number>;
+
+      template <int dim, typename Number = double>
+      using Indicator = EulerBarotropic::Indicator<dim, Number>;
+
+      template <int dim, typename Number = double>
+      using Limiter = EulerBarotropic::Limiter<dim, Number>;
+
+      template <int dim, typename Number = double>
+      using RiemannSolver = EulerBarotropic::RiemannSolver<dim, Number>;
+    };
+  } // namespace EulerBarotropic
+} // namespace ryujin
