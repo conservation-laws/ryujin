2e9379b1a8ff5084cb08eb11734472668f03822d
Matthias Maier <tamiko@43-1.org>
2025-10-15 15:06:34 -0500

    ParabolicModule: remove proxy class and put using declaration in place

--- source/parabolic_module.h
+++ source/parabolic_module.h
@@ -6,171 +6,18 @@
 #pragma once
 
 #include <compile_time_options.h>
-
-#include "convenience_macros.h"
-#include "hyperbolic_module.h"
-#include "initial_values.h"
-#include "mpi_ensemble.h"
-#include "offline_data.h"
-
-#include <deal.II/base/parameter_acceptor.h>
-#include <deal.II/base/timer.h>
-#include <deal.II/lac/sparse_matrix.templates.h>
-#include <deal.II/lac/vector.h>
-
-#include <functional>
+#include <description.h>
 
 namespace ryujin
 {
   /**
-   * Implicit backward Euler time-stepping and Crank-Nicolson time-stepping
-   * for the parabolic subsystem.
+   * Compatibility using declaration that selects the correct
+   * ParabolicModule class type from the equation-specific Description
+   * proxy class.
    *
    * @ingroup ParabolicModule
    */
   template <typename Description, int dim, typename Number = double>
-  class ParabolicModule final : public dealii::ParameterAcceptor
-  {
-  public:
-    /**
-     * @name Typedefs and constexpr constants
-     */
-    //@{
-
-    using HyperbolicSystem = typename Description::HyperbolicSystem;
-
-    using View =
-        typename Description::template HyperbolicSystemView<dim, Number>;
-
-    using ParabolicSystem = typename Description::ParabolicSystem;
-
-    using ParabolicSolver =
-        typename Description::template ParabolicSolver<dim, Number>;
-
-    using StateVector = typename View::StateVector;
-
-    //@}
-    /**
-     * @name Constructor and setup
-     */
-    //@{
-
-    /**
-     * Constructor.
-     */
-    ParabolicModule(
-        const MPIEnsemble &mpi_ensemble,
-        std::map<std::string, dealii::Timer> &computing_timer,
-        const OfflineData<dim, Number> &offline_data,
-        const HyperbolicSystem &hyperbolic_system,
-        const ParabolicSystem &parabolic_system,
-        const InitialValues<Description, dim, Number> &initial_values,
-        const std::string &subsection = "/ParabolicModule");
-
-    /**
-     * Prepare time stepping. A call to @p prepare() allocates temporary
-     * storage and is necessary before any of the following time-stepping
-     * functions can be called.
-     */
-    void prepare();
-
-    //@}
-    /**
-     * @name Functons for performing explicit time steps
-     */
-    //@{
-
-    /**
-     * This function preprocesses a given state vector @p U in preparation
-     * for a high order IMEX time step. This function exists because some
-     * time stepping variants have to precompute quantities before we can
-     * perform an IMEX step. In addition, this function is called whenever
-     * we perform a mesh transfer or output operation.
-     */
-    void prepare_state_vector(StateVector &state_vector, Number t) const;
-
-    /**
-     * Given a reference to a previous state vector @p old_U at time
-     * @p old_t and a time-step size @p tau perform an implicit backward
-     * euler step (and store the result in @p new_U).
-     *
-     * The function takes an optional array of states @p stage_U together
-     * with a an array of weights @p stage_weights to construct a modified
-     * high-order right-hand side / flux.
-     */
-    template <int stages>
-    void
-    backward_euler_step(const StateVector &old_state_vector,
-                        const Number old_t,
-                        std::array<std::reference_wrapper<const StateVector>,
-                                   stages> stage_state_vectors,
-                        const std::array<Number, stages> stage_weights,
-                        StateVector &new_state_vector,
-                        Number tau) const;
-
-    /**
-     * Given a reference to a previous state vector @p old_U at time @p
-     * old_t and a time-step size @p tau perform an implicit Crank-Nicolson
-     * step (and store the result in @p new_U).
-     *
-     * This variant is used in the TimeIntegrator class for the Strang
-     * split variants.
-     */
-    void crank_nicolson_step(const StateVector &old_state_vector,
-                             const Number old_t,
-                             StateVector &new_state_vector,
-                             Number tau) const;
-
-    /**
-     * Print a status line with solver statistics. This function is used
-     * for constructing the status message displayed periodically in the
-     * TimeLoop.
-     */
-    void print_solver_statistics(std::ostream &output) const;
-
-    //@}
-    /**
-     * @name Accessors
-     */
-    //@{
-    /**
-     * The number of restarts signalled by the step() function.
-     */
-    ACCESSOR_READ_ONLY(n_restarts)
-
-
-    /**
-     * The number of corrections performed by the step() function. This
-     * function exists to mirror the ParabolicModule interface and will
-     * always return 0.
-     */
-    ACCESSOR_READ_ONLY(n_corrections)
-
-    /**
-     * The number of ID violation warnings encounterd in the step()
-     * function.
-     */
-    ACCESSOR_READ_ONLY(n_warnings)
-
-    // FIXME: refactor to function
-    mutable IDViolationStrategy id_violation_strategy_;
-
-  private:
-    //@}
-    /**
-     * @name Internal data
-     */
-    //@{
-
-    // FIXME: refactor contents into this class.
-    ParabolicSolver parabolic_solver_;
-    mutable unsigned int cycle_;
-
-    mutable unsigned int n_restarts_;
-    mutable unsigned int n_corrections_;
-    mutable unsigned int n_warnings_;
-
-    //@}
-  };
-
+  using ParabolicModule =
+      typename Description::template ParabolicModule<dim, Number>;
 } /* namespace ryujin */
