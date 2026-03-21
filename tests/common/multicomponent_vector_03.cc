b805418ea203f540b9be98b146cabb2ffb12f4a9
Matthias Maier <tamiko@43-1.org>
2026-03-21 18:35:50 -0500

    MultiComponentVector: add a test

---
+++ tests/common/multicomponent_vector_03.cc
@@ -0,0 +1,77 @@
+#include <multicomponent_vector.h>
+
+int main(int argc, char *argv[])
+{
+  dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv);
+
+  /* Set up locally owned and relevant index sets. */
+
+  dealii::IndexSet locally_owned(12);
+  dealii::IndexSet locally_relevant(12);
+  locally_owned.add_range(0, 12);
+  locally_relevant.add_range(0, 12);
+
+  const auto scalar_partitioner =
+      std::make_shared<dealii::Utilities::MPI::Partitioner>(
+          locally_owned, locally_relevant, MPI_COMM_WORLD);
+
+  const auto vector_partitioner =
+      ryujin::Vectors::create_vector_partitioner(scalar_partitioner, 4);
+
+  ryujin::Vectors::MultiComponentVector<double, 4> state_vector;
+
+  state_vector.reinit_with_vector_partitioner(vector_partitioner);
+
+  using HostSpace = dealii::MemorySpace::Host;
+  using DefaultSpace = dealii::MemorySpace::Default;
+
+  const auto print_status = [&]() {
+    std::cout << "HostSpace active == "
+              << state_vector.is_active_memory_space<HostSpace>() << std::endl
+              << "DefaultSpace active == "
+              << state_vector.is_active_memory_space<DefaultSpace>()
+              << std::endl;
+  };
+
+  print_status();
+
+  for (unsigned int i = 0; i < 12; ++i) {
+    dealii::Tensor<1, 4, double> tensor{{static_cast<double>(10 * i),
+                                         static_cast<double>(10 * i + 1),
+                                         static_cast<double>(10 * i + 2),
+                                         static_cast<double>(10 * i + 3)}};
+    state_vector.write_tensor<double>(tensor, i);
+  }
+
+  std::cout << "\nState vector:\n";
+  for (unsigned int i = 0; i < 8; i += 1) {
+    std::cout << state_vector.read_tensor<double>(i) << "\n";
+  }
+
+  std::cout << "After move to DefaultSpace:" << std::endl;
+  state_vector.move_to_memory_space<DefaultSpace>();
+  print_status();
+  std::cout << "After repeated move to DefaultSpace:" << std::endl;
+  state_vector.move_to_memory_space<DefaultSpace>();
+  print_status();
+
+  const auto &view = state_vector.template get_view<DefaultSpace>();
+  using ExecutionSpace = DefaultSpace::kokkos_space::execution_space;
+  const auto exec = ExecutionSpace{};
+  Kokkos::parallel_for("test",
+                       Kokkos::RangePolicy<ExecutionSpace>(exec, 0, 3),
+                       [=](std::size_t i) {
+                         auto a = view.read_tensor(i);
+                         a *= 10.;
+                         view.write_tensor(a, i);
+                       });
+
+  std::cout << "After move to HostSpace:" << std::endl;
+  state_vector.move_to_memory_space<HostSpace>();
+  print_status();
+
+  std::cout << "\nState vector:\n";
+  for (unsigned int i = 0; i < 8; i += 1) {
+    std::cout << state_vector.read_tensor<double>(i) << "\n";
+  }
+}
