cb87eba90edaf3b8d6296c32681d2743bb8f5b7f
Nichakan Loesatapornpipit <nichakan@tamu.edu>
2026-07-10 12:28:43 -0500

    EulerAEOS: implement specific_entropy() and cold_curve_bound() for polytropic gas

--- source/euler_aeos/equation_of_state_polytropic_gas.h
+++ source/euler_aeos/equation_of_state_polytropic_gas.h
@@ -42,6 +42,15 @@ namespace ryujin
             "gas constant R", R_, "The specific gas constant R");
 
         cv_ = R_ / (gamma_ - 1.);
+
+        s0_ = 0.;
+        this->add_parameter("reference specific entropy",
+                            s0_,
+                            "The reference specific entropy");
+
+        /* Update the EOS interpolation parameters on parameter read in: */
+        ParameterAcceptor::parse_parameters_call_back.connect(
+            [this] { cv_ = R_ / (gamma_ - 1.); });
       }
 
       /**
@@ -78,11 +87,37 @@ namespace ryujin
       }
 
       /**
+       * The cold curve bound is given by
+       * \f{align}
+       *   e_cold = 0
+       * \f}
+       */
+      double cold_curve_bound(double /*rho*/) const final
+      {
+        return 0.;
+      }
+
+      /**
+       * The specific entropy is given by
+       * \f{align}
+       *   s = cv * \ln((gamma - 1) \rho e) -
+       *   cv gamma \ln((gamma - 1) cv \rho) + s0
+       * \f}
+       */
+      double specific_entropy(double rho, double e) const final
+      {
+        const auto gm1 = gamma_ - 1.;
+        const auto p = gm1 * rho * e;
+        return cv_ * (std::log(p) - gamma_ * std::log(gm1 * cv_ * rho)) + s0_;
+      }
+
+      /**
        * The speed of sound is given by
        * \f{align}
        *   c^2 = \gamma * (\gamma - 1) e
        * \f}
        */
+
       double speed_of_sound(double /*rho*/, double e) const final
       {
         return std::sqrt(gamma_ * (gamma_ - 1.) * e);
@@ -92,6 +127,7 @@ namespace ryujin
       double gamma_;
       double R_;
       double cv_;
+      double s0_;
     };
   } // namespace EquationOfStateLibrary
 } // namespace ryujin
