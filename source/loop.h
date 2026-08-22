2bf65f88893731d6fffad2ff4d71948a9f534504
Matthias Maier <tamiko@43-1.org>
2026-08-22 04:57:48 +0000

    loop.h: add a gpu_loop() driver running a Kokkos parallel_for

--- source/loop.h
+++ source/loop.h
@@ -10,9 +10,11 @@
 #include <simd.h>
 
 #include <deal.II/base/config.h>
+#include <deal.II/base/memory_space.h>
 #include <deal.II/base/parallel.h>
 
 #include <string>
+#include <type_traits>
 
 #ifdef WITH_OPENMP
 #include <omp.h>
@@ -112,5 +114,105 @@ namespace ryujin
         body(ScalarNumber(), std::forward<Args>(args)..., i);
     }
 #endif
+  }
+
+
+  /*
+   * A loop running on the device (i.e., in the default memory space). The
+   * loop traverses the index range [left, right) with a suitable Kokkos
+   * parallel_for using a range policy.
+   *
+   * @note The index range [left, internal) that is used for SIMD
+   * vectorization in cpu_simd_loop() is currently ignored: On the device
+   * every "lane" operates on a scalar value and the loop body is thus
+   * always called with a scalar sentinel type.
+   *
+   * @note Here, @p body is a functor that must accept a "sentinel" type as
+   * first argument and the current index i as last argument. Additional
+   * `args` may be specified in the gpu_loop() invocation that will be
+   * forwarded to the loop body:
+   * `body(ScalarNumber(), std::forward<Args>(args)..., i);`
+   *
+   * @note The loop body (and everything it references) has to be callable
+   * on the device. In particular, @p body and all @p args are copied into
+   * the kernel, meaning that the functor must capture by value and must be
+   * trivially copyable.
+   *
+   * @note The function fences the execution space before returning. It
+   * thus has the same (synchronous) semantics as cpu_simd_loop().
+   */
+  template <typename ScalarNumber, typename Functor, typename... Args>
+  inline void gpu_loop(const std::string &region_name,
+                       const Functor &body,
+                       const unsigned int left,
+                       const unsigned int internal [[maybe_unused]],
+                       const unsigned int right,
+                       Args &&...args)
+  {
+    Assert(left <= internal && internal <= right,
+           dealii::ExcMessage("Invalid index range: it must hold left <= "
+                              "internal, internal <= right"));
+
+    using MemorySpace = dealii::MemorySpace::Default;
+    using ExecutionSpace = typename MemorySpace::kokkos_space::execution_space;
+    using Policy =
+        Kokkos::RangePolicy<ExecutionSpace, Kokkos::IndexType<unsigned int>>;
+
+    const auto exec = ExecutionSpace{};
+
+    Kokkos::parallel_for(
+        region_name,
+        Policy(exec, left, right),
+        KOKKOS_LAMBDA(const unsigned int i) {
+          body(ScalarNumber(), args..., i);
+        });
+
+    exec.fence();
+  }
+
+
+  /*
+   * A loop running either on the CPU, or on the device depending on the
+   * selected memory space: For dealii::MemorySpace::Host the loop is
+   * dispatched to cpu_simd_loop(), and for dealii::MemorySpace::Default to
+   * gpu_loop().
+   *
+   * @note Here, @p body is a functor that must accept a "sentinel" type as
+   * first argument and the current index i as last argument. Additional
+   * `args` may be specified in the loop() invocation that will be forwarded
+   * to the loop body.
+   */
+  template <typename MemorySpace,
+            typename ScalarNumber,
+            typename Functor,
+            typename... Args>
+  inline void loop(const std::string &region_name,
+                   const Functor &body,
+                   const unsigned int left,
+                   const unsigned int internal,
+                   const unsigned int right,
+                   Args &&...args)
+  {
+    using HostSpace = dealii::MemorySpace::Host;
+    using DefaultSpace = dealii::MemorySpace::Default;
+    static_assert(std::is_same_v<MemorySpace, HostSpace> ||
+                      std::is_same_v<MemorySpace, DefaultSpace>,
+                  "Unexpected memory space");
+
+    if constexpr (std::is_same_v<MemorySpace, HostSpace>) {
+      cpu_simd_loop<ScalarNumber>(region_name,
+                                  body,
+                                  left,
+                                  internal,
+                                  right,
+                                  std::forward<Args>(args)...);
+    } else {
+      gpu_loop<ScalarNumber>(region_name,
+                             body,
+                             left,
+                             internal,
+                             right,
+                             std::forward<Args>(args)...);
+    }
   }
 } // namespace ryujin
