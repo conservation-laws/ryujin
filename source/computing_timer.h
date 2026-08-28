ffbe021f8fd7f4409da5ac640cd63d66cf889281
Matthias Maier <tamiko@43-1.org>
2026-08-28 20:14:03 +0000

    computing_timer.h: add a ComputingTimer class

--- source/computing_timer.h
+++ source/computing_timer.h
@@ -5,12 +5,108 @@
 
 #pragma once
 
+#include "instrumentation.h"
 #include <compile_time_options.h>
 #include <convenience_macros.h>
 #include <simd.h>
 
+#include <deal.II/base/timer.h>
+
+#include <map>
+#include <string>
+
+#ifdef DEBUG_OUTPUT
+#include <iostream>
+#endif
+
 namespace ryujin
 {
+  /**
+   * An accumulator for the wall and CPU time spent in named sections of
+   * the host code, keyed by section name.
+   *
+   * @note This class is a singleton and not thread safe.
+   *
+   * @ingroup Miscellaneous
+   */
+  class ComputingTimer
+  {
+  public:
+    /**
+     * A RAII scope for the timer of the specified @p section.
+     *
+     * The constructor of the class starts a timer with the specified name.
+     * If ryujin is configured with likwid then likwid instrumentation will
+     * also be started with the given section name. The destructor of the
+     * class stops the timer again and also stops likwid instrumentation.
+     *
+     * @note This class does not perform MPI synchronization in contrast to
+     * the deal.II counterpart.
+     */
+    class Scope
+    {
+    public:
+      /**
+       * Constructor. Starts a timer for the selected @p section.
+       */
+      Scope(const std::string &section)
+          : section_(section)
+      {
+        LIKWID_MARKER_START(section_.c_str());
+        timers_[section_].start();
+#ifdef DEBUG_OUTPUT
+        std::cout << "{scoped timer} \"" << section_ << "\" started"
+                  << std::endl;
+#endif
+      }
+
+      /**
+       * Destructor. Stops the timer.
+       */
+      ~Scope()
+      {
+#ifdef DEBUG_OUTPUT
+        std::cout << "{scoped timer} \"" << section_ << "\" stopped"
+                  << std::endl;
+#endif
+        timers_[section_].stop();
+        LIKWID_MARKER_STOP(section_.c_str());
+      }
+
+    private:
+      const std::string section_;
+    };
+
+    /**
+     * Return a reference to the deal.II timer object for the given
+     * @p section, creating it if necessary.
+     */
+    static dealii::Timer &timer(const std::string &section)
+    {
+      return timers_[section];
+    }
+
+    /**
+     * Return a const reference to the full map of named timers.
+     */
+    static const std::map<std::string, dealii::Timer> &timers()
+    {
+      return timers_;
+    }
+
+    /**
+     * Reset the timer object back to zero
+     */
+    static void reinit()
+    {
+      timers_.clear();
+    }
+
+  private:
+    static inline std::map<std::string, dealii::Timer> timers_;
+  };
+
+
   /*
    * An accumulator for the wall time spent executing compute kernels on the
    * device.
